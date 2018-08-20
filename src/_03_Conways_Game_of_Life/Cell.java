b6324a16825023790b3666d3a17ae22d6279ad7e
Chloe Luwa <chloeluwa@yahoo.com>
2018-08-20 16:55:47 -0700

    Conway's Game of Life(IP)
    Conway's Game of Life(IP)
    8/20/18

--- src/_03_Conways_Game_of_Life/Cell.java
+++ src/_03_Conways_Game_of_Life/Cell.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 public class Cell implements Drawable{
 	public boolean isAlive = false;
 	
+	/*Added this*/int neighbors;
+	
 	private int x;
 	private int y;
 
@@ -17,16 +19,16 @@ public class Cell implements Drawable{
 		this.cellSize = size;
 	}
 	
-	//11. Complete tue liveOrDie method
+	//11. Complete the liveOrDie method
 	//    It sets isAlive to true or false based on the neighbors and 
 	//the rules of the game
 	/*
-	 * 1. Any live cell with fewer than two live nieghbours dies, as if caused by underpopulation.
-	 * 2. Any live cell with two or three live neighbours lives on to the next generation.
-	 * 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
-	 * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-	 * (source: Wikipedia)
-	 * */
+	  1. Any live cell with fewer than two live nieghbours dies, as if caused by underpopulation.
+	  2. Any live cell with two or three live neighbours lives on to the next generation.
+	  3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+	  4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+	  (source: Wikipedia)
+	  */
 	public void liveOrDie(int numNeighbors) {
 		
 	}
